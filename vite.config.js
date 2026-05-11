e54096497be0ff6f2b5e4c79205c0462094ed7d3
paula-spila <paula.spila@gmail.com>
2026-05-12 00:33:49 +0300

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
-    base: env.VITE_BASE || "/",
+    base: "/BakalauraDarbs",
     server: {
       port: 5173,
       strictPort: false,
